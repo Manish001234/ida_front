4dbc9e6b560180c5b4e75107cc2c404a8dd06d14
Manish Reddy <89566773+Manish001234@users.noreply.github.com>
2023-07-20 15:32:21 +0530

    add

--- src/features/Authentication/login.jsx
+++ src/features/Authentication/login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <main className="w-full max-w-md mx-auto p-6">
+<main className="w-full max-w-md mx-auto p-6">
   <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
     <div className="p-4 sm:p-7">
       <div className="text-center">
@@ -171,6 +171,7 @@ const Login = () => {
     </div>
   </div>
 </main>
+
 
     </div>
   )
